72b681f7f99609ad85299bfcb321e7a3cd5ce20b
Alex Tavares <amtavares@wpi.edu>
2020-03-05 22:08:46 -0500

    small changes

--- Bomberman/group15/q_entity.py
+++ Bomberman/group15/q_entity.py
@@ -6,14 +6,23 @@ from sensed_world import SensedWorld
 
 class qEntity(CharacterEntity):
 
-    def __init__(self):
-        CharacterEntity.__init__()
+    def __init__(self, name, avatar, x, y, qLearner, iterNum, maxIterations, bombs = True, trainModel=False):
+        CharacterEntity.__init__(name, avatar, x, y)
         #Includes other variables needed for q learning
+        self.qLearner = qLearner
+        self.trainModel = trainModel
+        self.iterNum = iterNum
+        self.maxIterations = maxIterations
+        self.bombs = bombs
 
     def do(self):
-        pass
+        if self.trainModel:
+            pass
+        else:
+            pass
 
-    def updateWeights(self):
+    #updates weights
+    def done(self):
         pass
 
 
--- Bomberman/group15/q_learner.py
+++ Bomberman/group15/q_learner.py
@@ -1,12 +1,23 @@
 
 
+#Q Learner model
 class qLearner:
 
-    def __init__(self):
+    def __init__(self, weights, heuristics, gamma=0.9, learning_rate = 0.2):
+        self.weights = weights
+        self.heuristics = heuristics
+        self.gamma = gamma
+        self.lr = learning_rate
+        moves = []
+
         pass
 
     def best_move(self):
         pass
 
     def updateWeights(self):
+        pass
+
+
+    def approximate_Q_value(self, world, character):
         pass
