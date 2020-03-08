91c7ef1904a8cc8c2fb726ad6caa1e62cabd41ee
Alex Tavares <amtavares@wpi.edu>
2020-03-08 17:06:53 -0400

    added to q learner

--- Bomberman/group15/q_entity.py
+++ Bomberman/group15/q_entity.py
@@ -8,30 +8,27 @@ from sensed_world import SensedWorld
 
 class qEntity(CharacterEntity):
 
-    def __init__(self, name, avatar, x, y, qLearner, iterNum, maxIterations, bombs = True, trainModel=False):
+    def __init__(self, name, avatar, x, y, qLearner, iterNum, maxIterations, trainModel=False):
         CharacterEntity.__init__(name, avatar, x, y)
         #Includes other variables needed for q learning
         self.qLearner = qLearner
         self.trainModel = trainModel
         self.iterNum = iterNum
         self.maxIterations = maxIterations
-        self.bombs = bombs
-        self.previousWorld = None
+        self.oldWorld = None
 
 
     def do(self, world):
-        self.previousWorld = world
+        self.oldWorld = world
         randomChance = 1 / math.sqrt(self.iterNum + 1)
-        if self.trainModel and random.random() < self.randomChance:
+        if self.trainModel and random.random() < randomChance:
             moves = [-1,0,1]
 
             dx = random.choice(moves)
             dy = random.choice(moves)
-
-            if self.bombs:
-                bombs = random.choice(moves[1:2])
-                if bombs == 1:
-                    self.placeBomb()
+            bombs = random.choice(moves[1:])
+            if bombs == 1:
+                self.placeBomb()
             self.move(dx,dy)
         else:
             # call Q-Learner
--- Bomberman/group15/q_learner.py
+++ Bomberman/group15/q_learner.py
@@ -1,4 +1,6 @@
-
+sys.path.insert(0, '../bomberman')
+from entity import CharacterEntity
+from sensed_world import SensedWorld
 
 #Q Learner model
 class qLearner:
@@ -8,17 +10,52 @@ class qLearner:
         self.heuristics = heuristics
         self.gamma = gamma
         self.lr = learning_rate
-        moves = []
+        self.possibleMoves = []
 
+    def getAvailableMoves(self, world, character):
         pass
 
-    def best_move(self):
-        pass
+    def best_move(self, world, character):
+        availableMoves = self.getAvailableMoves(world, character)
+        best_q = -9999
+        best_action = (0,0,0)
 
-    def updateWeights(self):
-        pass
+        for move in availableMoves:
+            #generate new world
+            new_world = SensedWorld.from_world(world)
+
+            #check if character is still alive
+            if new_world.me(character) is None:
+                break
+
+            #make move
+            new_world.me(character).move(move[0], move[1])
+            if move[3] == 1:
+                new_world.me(character).place_bomb()
+            new_world, events = new_world.next()
+
+            #check if character is dead and why
+            if new_world.me(character) is None:
+                for event in events:
+                    # check what type of event
+                    pass
+
+
+
+
+
+    def updateWeights(self, oldWorld, newWorld, character, reward):
+        delta = (reward + self.gamma*self.approximate_Q_value(newWorld))-self.approximate_Q_value(oldWorld)
+
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i] + self.lr*delta*self.heuristics[i](oldWorld, character)
 
 
     def approximate_Q_value(self, world, character):
         sum = 0
+
+        for i in range(len(self.heuristics)):
+            sum += self.weights[i]*self.heuristics[i](world, character)
+
+        return sum
 
